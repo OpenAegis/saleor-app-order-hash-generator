8ab64407eae2d9e478704b59eb5a663888624c7f
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-10 17:15:12 +0100

    Add comment

--- server/api/utils.ts
+++ server/api/utils.ts
@@ -1,5 +1,10 @@
 import { Context } from "hono";
 
+/**
+ * Hono handler signature is Context => Response
+ * In order to use @saleor/app-sdk methods we need to unpack raw Web API Request object
+ * and pass it to app-sdk handler function
+ */
 export function unpackHonoRequest(
   handlerFn: (req: Request) => Promise<Response> | Response,
 ) {
