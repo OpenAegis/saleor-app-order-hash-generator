388b13b12d4625272a313a6a17642bc2910980c8
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-10 15:44:40 +0100

    Change APL config to avoid deployment issues

--- server/saleor-app.ts
+++ server/saleor-app.ts
@@ -12,15 +12,14 @@ import { DenoAPL } from "./deno-kv-apl.ts";
 let apl: APL;
 
 switch (Deno.env.get("APL")) {
-  case "deno":
-    apl = new DenoAPL();
-    break;
   case "upstash":
     // Require `UPSTASH_URL` and `UPSTASH_TOKEN` environment variables
     apl = new UpstashAPL();
     break;
+  case "deno":
   default:
-    throw new Error("Cannot find valid APL");
+    apl = new DenoAPL();
+    break;
 }
 
 export const saleorApp = new SaleorApp({ apl });
